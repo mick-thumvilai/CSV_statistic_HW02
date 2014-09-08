986813b1525feea46cd6f116c429281cdffa723e
mick-thumvilai <lucifer_maxzo@hotmail.com>
2014-09-08 10:28:12 +0700

    first commit

---
+++ csv_stat.rb
@@ -0,0 +1,75 @@
+require 'csv'
+csv = Array.new
+name = Array.new
+marks = Array.new
+color = Array.new
+seperated = Array.new
+colorScore = [0,0,0]
+favoriteColor = ""
+badColor = ""
+maxScore = 0
+minScore = 100
+mean = 0
+i = 1
+CSV.foreach('data.csv') do |row|
+	csv << row
+end
+while i<csv.length
+	name << csv[i][0]
+	marks << csv[i][1].to_f
+	color << csv[i][2]
+	i += 1
+end
+
+marks.each do |x|
+	if x>maxScore
+		maxScore = x
+	end
+	if x<minScore
+		minScore = x
+	end
+	mean += x
+end
+mean = mean/marks.length
+
+color.each do |x|
+	if x == "Blue"
+		colorScore[0] += 1
+	end
+	if x == "Yellow"
+		colorScore[1] += 1
+	end
+	if x == "Red"
+		colorScore[2] += 1
+	end
+end
+
+if (colorScore[0]>colorScore[1]) && (colorScore[0]>colorScore[2])
+	favoriteColor = "Blue"
+elsif (colorScore[1]>colorScore[0]) && (colorScore[1]>colorScore[2])
+	favoriteColor = "Yellow"
+else
+	favoriteColor = "Red"
+end
+
+if (colorScore[0]<colorScore[1]) && (colorScore[0]<colorScore[2])
+	badColor = "Blue"
+elsif (colorScore[1]<colorScore[0]) && (colorScore[1]<colorScore[2])
+	badColor = "Yellow"
+else
+	badColor = "Red"
+end
+
+i=0
+seperated = Array.new
+while i<name.length
+	name[i].downcase!
+	seperated << name[i].split(" ")
+end
+
+puts seperated
+puts favoriteColor
+puts badColor
+puts maxScore
+puts minScore
+puts mean
